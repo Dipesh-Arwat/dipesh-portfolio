85e34701b89bef29db124c433b068458e5a7d267
Dipesh Arwat <139738936+Dipesh-Arwat@users.noreply.github.com>
2025-02-27 10:58:33 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -54,16 +54,29 @@ export const config = {
     internships: {
       internship: [
         {
+          companyName: 'Ollato Eduversity Pvt Ltd',
+          role: 'Web Developer Intern',
+          introText: '',
+          duration: '01/01/2025 - 10/02/2025',
+          keyPointOne: 'Developed 2+ full-stack web applications using React.js & Node.js.',
+          keyPointTwo:
+            'Demonstrated strong technical skills in MERN stack development.',
+          keyPointThree: 'Proactively contributed to team projects and improved system efficiency.',
+          keyPointFour:
+            'Gained hands-on experience in authentication, API integration, and database management.',
+          offsetValue: '0',
+        },
+        {
           companyName: 'DC Info Tech Pvt Ltd',
           role: 'Web Developer Intern',
           introText: '',
-          duration: '17/09/2024 - Present',
-          keyPointOne: 'Engaged in a structured three-month self-learning internship, focusing on web development tasks as part of a detailed plan provided by the organization',
+          duration: '17/09/2024 - 17/12/2024',
+          keyPointOne: 'Successfully completed a three-month structured internship, focusing on web development tasks aligned with the organizations detailed plan.',
           keyPointTwo:
-            'Worked on various tasks and responsibilities related to front-end and back-end development, based on the Web Development Internship Tasks provided',
-          keyPointThree: 'Completed tasks such as implementing new features, debugging, and optimizing web applications, following best practices and company standards.',
+            'Worked on various tasks and responsibilities related to front-end and back-end development, based on Internship Tasks provided.',
+          keyPointThree: 'Developed and deployed real-world projects using modern web technologies such as React.js, Node.js and MongoDB.',
           keyPointFour:
-            'Gained hands-on experience in using modern web technologies such as React.js and Node.js in real-time projects.',
+            'Gained hands-on experience in user authentication, REST APIs, and database management completing 10+ tasks as part of theinternship plan.',
           offsetValue: '0',
         },
         {
